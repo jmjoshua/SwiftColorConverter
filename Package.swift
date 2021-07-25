0e7784800b1668d84b8fb74066c0d6f6de7e15ba
Josh Moore <jmjoshua7465@gmail.com>
2021-07-25 15:21:10 -0600

    Removed platforms

--- Package.swift
+++ Package.swift
@@ -5,9 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftColorConverter",
-    platforms: [
-        .macOS(.v10_10), .iOS(.v8), .tvOS(.v9)
-    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
