9afafd192eb3f01c202834feeddcb4507845ded9
Josh Moore <jmjoshua7465@gmail.com>
2021-07-25 14:24:30 -0600

    triangleForModel

--- Sources/SwiftColorConverter/SwiftColorConverter.swift
+++ Sources/SwiftColorConverter/SwiftColorConverter.swift
@@ -1,13 +1,43 @@
 import Foundation
 
-struct Triangle {
+struct Triangle: Equatable {
     var r: CGPoint
     var g: CGPoint
     var b: CGPoint
 }
 
 struct SwiftColorConverter {
-    var text = "Hello, World!"
+    var hueBulbs = [
+        "LCT001", /* Hue A19 */
+        "LCT002", /* Hue BR30 */
+        "LCT003" /* Hue GU10 */
+    ]
+    var livingColors: [String] = [
+        "LLC001", /* Monet, Renoir, Mondriaan (gen II) */
+        "LLC005", /* Bloom (gen II) */
+        "LLC006", /* Iris (gen III) */
+        "LLC007", /* Bloom, Aura (gen III) */
+        "LLC011", /* Hue Bloom */
+        "LLC012", /* Hue Bloom */
+        "LLC013", /* Storylight */
+        "LST001" /* Light Strips */
+    ]
+    
+    func triangleForModel(_ model: String) -> Triangle {
+        if hueBulbs.contains(model) {
+            return Triangle(r: CGPoint(x: 0.675, y: 0.322),
+                            g: CGPoint(x: 0.4091, y: 0.518),
+                            b: CGPoint(x: 0.167, y: 0.04))
+        } else if livingColors.contains(model) {
+            return Triangle(r: CGPoint(x: 0.704, y: 0.296),
+                            g: CGPoint(x: 0.2151, y: 0.7106),
+                            b: CGPoint(x: 0.138, y: 0.08))
+        } else {
+            return Triangle(r: CGPoint(x: 1, y: 0),
+                            g: CGPoint(x: 0, y: 1),
+                            b: CGPoint(x: 0, y: 0))
+        }
+    }
     
     func crossProduct(_ p1: CGPoint, _ p2: CGPoint) -> Float {
         return Float(p1.x * p2.y - p1.y * p2.x)
--- Tests/SwiftColorConverterTests/SwiftColorConverterTests.swift
+++ Tests/SwiftColorConverterTests/SwiftColorConverterTests.swift
@@ -8,6 +8,41 @@
             sut = SwiftColorConverter()
         }
         
+        // MARK: triangleForModel
+        func testTriangleForModel_hueBulb() {
+            let model = "LCT002"
+            let expectedResult = Triangle(r: CGPoint(x: 0.675, y: 0.322),
+                                          g: CGPoint(x: 0.4091, y: 0.518),
+                                          b: CGPoint(x: 0.167, y: 0.04))
+            
+            let result = sut.triangleForModel(model)
+            
+            XCTAssertEqual(result, expectedResult)
+        }
+        
+        func testTriangleForModel_livingColors() {
+            let model = "LLC013"
+            let expectedResult = Triangle(r: CGPoint(x: 0.704, y: 0.296),
+                                           g: CGPoint(x: 0.2151, y: 0.7106),
+                                           b: CGPoint(x: 0.138, y: 0.08))
+            
+            let result = sut.triangleForModel(model)
+            
+            XCTAssertEqual(result, expectedResult)
+        }
+        
+        func testTriangleForModel_unsupportedModel() {
+            let model = "unsupported"
+            let expectedResult = Triangle(r: CGPoint(x: 1, y: 0),
+                                          g: CGPoint(x: 0, y: 1),
+                                          b: CGPoint(x: 0, y: 0))
+            
+            let result = sut.triangleForModel(model)
+            
+            XCTAssertEqual(result, expectedResult)
+        }
+        
+        // MARK: crossProduct
         func testCrossProduct() {
             let point1 = CGPoint(x: 1, y: 2)
             let point2 = CGPoint(x: 2, y: 1)
@@ -26,6 +61,7 @@
             XCTAssertEqual(result, -1.0)
         }
         
+        // MARK: isPointInTrangle
         func testIsPointInTriagnleTrue() {
             let point = CGPoint(x: 2, y: 0)
             let r = CGPoint(x: -1, y: -1)
